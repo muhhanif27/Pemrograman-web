d833dd4e7513d102655ae7b36f9ea19eb90940b9
WINDOWS 11 <mhanif0203@gmail.com>
2024-11-12 22:43:14 +0700

    push

---
+++ MODUL4/CODELAB/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php 
+namespace app\Config;
+
+class DatabaseConfig
+{
+    public $host = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $database_name = "web_modul4";
+    public $port = "3306";
+}
